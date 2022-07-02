449604ea78a7fc182b235e76e5dba444cba3b0af
charity-bit <charitynyanchera@gmail.com>
2022-07-03 00:10:21 +0300

    merged with ft-tm-apis

--- app/serializers.py
+++ app/serializers.py
@@ -1,1 +1,30 @@
-# from rest_framework import fields, serializers
+from dataclasses import fields
+from importlib.metadata import files
+from django.test import modify_settings
+from rest_framework import serializers
+from .models import User,Profile,Comment,Module,Session,Announcement
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    technical_mentor = UserSerializer(read_only=True)
+    class Meta:
+        model = Module
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only = True) 
+    modules = ModuleSerializer(read_only =True )
+    class Meta:
+        model = Profile
+        fields = '__all'
+
+
+
+
+
+
