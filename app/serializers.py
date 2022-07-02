0e4375fefb6e6278fd0115931549e516e278be1b
charity-bit <charitynyanchera@gmail.com>
2022-07-03 00:20:19 +0300

    ft: models

--- app/serializers.py
+++ app/serializers.py
@@ -1,27 +1,25 @@
 from rest_framework import serializers
-from .models import User,Profile,Comment,Module,Session,Announcement
+from .models import User, Profile, Comment, Module, Session, Announcement
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ModuleSerializer(serializers.ModelSerializer):
     technical_mentor = UserSerializer(read_only=True)
+
     class Meta:
         model = Module
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only = True) 
-    modules = ModuleSerializer(read_only =True )
+    user = UserSerializer(read_only=True)
+    modules = ModuleSerializer(read_only=True)
+
     class Meta:
         model = Profile
-        fields = '__all__'
-
-
-
-
-
-
+        fields = "__all__"
