b86ea057474072f7e89daf183ac3e7a37119ead1
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-07-03 17:58:51 +0300

    ft:created coments api views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 # from django.shortcuts import render
-# from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view
 from .serializers import UserSerializer,ModuleSerializer,ProfileSerializer,SessionSerializer,CommentSerializer
 from rest_framework.response import Response
 from app.models import User,Module,Profile,Session,Announcement,Comment
@@ -22,3 +22,20 @@ from rest_framework.authtoken.models import Token
 
 def api(request):
     return HttpResponse('this is the backed for classroom schedule')
+
+
+
+
+@api_view(['POST']) 
+def create_comment(request,format=None):
+    serializers=CommentSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data,status=status.HTTP_201_CREATED)
+    return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def all_comments(request,format=None):
+    comments=Comment.objects.all()
+    serializers=CommentSerializer(comments,many=True)
+    return Response(serializers.data)
